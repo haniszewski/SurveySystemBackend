8d10545f16b61d5830410d4d9810814ee1bcd236
kamil.haniszewski <kamil.haniszewski@systemeg.net>
2023-10-06 17:57:15 +0200

    initial commit

---
+++ controllers/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Survey(models.Model):
+    name = models.CharField(max_length=255, help_text="Enter the name of the survey")
+    start_date = models.DateField(help_text="Enter the start date of the survey")
+    end_date = models.DateField(help_text="Enter the end date of the survey")
+
+
+class QuestionType(models.Model):
+    name = models.CharField(max_length=255, help_text="Enter the type of question (e.g. Multiple Choice, Text, Rating)")
+
+class Form(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name='forms')
+    question_type = models.ForeignKey(QuestionType, on_delete=models.CASCADE, related_name='forms')
+    order = models.IntegerField()
+    question_text = models.TextField(help_text="Enter the text for the question")
+
+
+class Choice(models.Model):
+    form = models.ForeignKey(Form, on_delete=models.CASCADE, related_name='choices')
+    choice_text = models.CharField(max_length=255, help_text="Enter the text for this choice")
+
+class AnswerCheckBox(models.Model):
+    value = models.ForeignKey(Choice, on_delete=models.CASCADE, related_name='answerscheckbox')
