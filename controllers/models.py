733ba83f256d8dd992632d320fab65d4a5627a17
kamil.haniszewski <kamil.haniszewski@systemeg.net>
2023-10-13 17:10:23 +0200

    added extra model TODO add User / derive from djnagos

--- controllers/models.py
+++ controllers/models.py
@@ -5,26 +5,41 @@ class Survey(models.Model):
     name = models.CharField(max_length=255, help_text="Enter the name of the survey")
     start_date = models.DateField(help_text="Enter the start date of the survey")
     end_date = models.DateField(help_text="Enter the end date of the survey")
+    salt = models.CharField(max_length=255, help_text="abc",default="abc")
+    
+class SurveyPermissions(models.Model):
+    name = models.CharField(max_length=255, help_text="Enter the name of the survey")
+    
 
 class SurveyOwners(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     survey = models.ForeignKey(Survey,on_delete=models.PROTECT)
-    permission = models.IntegerField(default=0)
+    permissions = models.ForeignKey(SurveyPermissions,on_delete=models.PROTECT)
+    
+class SurveyParticipants(models.Model):
+    survey = models.ForeignKey(Survey,on_delete=models.PROTECT)
+    email_crypt = models.CharField(max_length=255, help_text="")
 
 
-class QuestionType(models.Model):
+class InputType(models.Model):
     name = models.CharField(max_length=255, help_text="Enter the type of question (e.g. Multiple Choice, Text, Rating)")
 
-class Form(models.Model):
+class FormInput(models.Model):
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name='forms')
-    question_type = models.ForeignKey(QuestionType, on_delete=models.CASCADE, related_name='forms')
+    question_type = models.ForeignKey(InputType, on_delete=models.CASCADE, related_name='forms')
     order = models.IntegerField()
     question_text = models.TextField(help_text="Enter the text for the question")
 
 
-class Choice(models.Model):
-    form = models.ForeignKey(Form, on_delete=models.CASCADE, related_name='choices')
-    choice_text = models.CharField(max_length=255, help_text="Enter the text for this choice")
+class FormInputChoice(models.Model):
+    form = models.ForeignKey(FormInput, on_delete=models.CASCADE, related_name='choices')
+    option_text = models.CharField(max_length=255, help_text="Enter the text for this choice",null=True)
+    
 
-class AnswerCheckBox(models.Model):
-    value = models.ForeignKey(Choice, on_delete=models.CASCADE, related_name='answerscheckbox')
+class SurveyParticipantAnswer(models.Model):
+    choice = models.ForeignKey(FormInputChoice, on_delete=models.CASCADE, related_name='answerscheckbox', null=True)
+    value_float = models.FloatField(null=True)
+    value_int = models.IntegerField(null=True)
+    value_text = models.TextField(null=True)
+    participant = models.ForeignKey(SurveyParticipants, on_delete=models.PROTECT, null=False)
+    
