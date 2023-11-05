69abfef6c7c8329db01b1f35900d24ceed9e10fd
kamil.haniszewski <kamil.haniszewski@systemeg.net>
2023-11-05 15:29:07 +0100

    added controller to handle adding survey TODO current user as owner

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,26 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from controllers.models import *
+
+
+class FormInputChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FormInputChoice
+        fields = ('option_text',)
+
+class FormInputSerializer(serializers.ModelSerializer):
+    choices = FormInputChoiceSerializer(many=True, required=False)
+    
+    class Meta:
+        model = FormInput
+        fields = ('question_type', 'order', 'question_text', 'choices')
+
+class SurveySerializer(serializers.ModelSerializer):
+    forms = FormInputSerializer(many=True)
+    
+    class Meta:
+        model = Survey
+        fields = ('name', 'start_date', 'end_date', 'forms')
 
 class CreateAccountSerializer(serializers.Serializer):
     email = serializers.EmailField()
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from typing import Dict, Any
 from django.db import IntegrityError
 from django.http.response import HttpResponse
 from django.views.decorators.http import require_http_methods
@@ -6,10 +7,11 @@ from django.views.decorators.http import require_http_methods
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import CreateAccountSerializer
+from .serializers import *
 from django.contrib.auth.models import User
+from django.db import transaction
 
-# Create your views here.
+
 
 @require_http_methods(["GET","POST"])
 def hello_world(request):
@@ -29,10 +31,39 @@ class CreateAccountView(APIView):
             
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-      
 class CreateSurveyView(APIView):
-    def put(self,request):
-        return HttpResponse("ok")
+    
+    def post(self, request, *args, **kwargs):
+        serializer = SurveySerializer(data=request.data)
+        if serializer.is_valid():
+            # Start Database transaction
+            with transaction.atomic():
+                survey = Survey.objects.create(
+                    name=serializer.validated_data['name'],
+                    start_date=serializer.validated_data['start_date'],
+                    end_date=serializer.validated_data['end_date']
+                )
+                for form_data in serializer.validated_data['forms']:
+                    form_input = FormInput.objects.create(
+                        survey=survey,
+                        question_type=form_data['question_type'],
+                        order=form_data['order'],
+                        question_text=form_data['question_text']
+                    )
+                    
+                    # If choices empty throw error
+                    for choice_data in form_data['choices']:
+                        FormInputChoice.objects.create(
+                            form=form_input,
+                            option_text=choice_data['option_text']
+                        )
+                
+                # TODO add current user as owner of survey
+                # SurveyOwners.objects.create(user=request.user, survey=survey)
+                
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class GetAllSurveyByOwnerView(APIView):
     def get(self,request):
